b69e04a0541e518af15c012ff4b5049d5b5fceca
Oliveseater <nslivnicina@gmail.com>
2022-01-31 11:03:10 +0500

    push sub

--- again13wow/again13wow/Program.cs
+++ again13wow/again13wow/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("вау, тут будет конфликт в будущем, ага");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число а - ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b - ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1 +\n2 -\n3 *\n4 /");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+
+                case 2:
+                    Console.WriteLine("Ответ: " + (a - b));
+                    break;
+
+                    break;
+                default: Console.WriteLine("wrong"); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
